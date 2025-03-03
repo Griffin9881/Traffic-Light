8cd468394e9917c4db23ee1e736fa8ee2b4cdcbc
Griffin <lamb.griffin@icloud.com>
2025-03-03 11:59:52 -0500

    deleted comments and added blinks

--- sketch.ino
+++ sketch.ino
@@ -65,6 +65,33 @@ static int ledController(struct pt *pt) {
     case 45:
       setLED(0, 1, 0);
       break;
+    // BLINK YELLOW
+    case 49:
+      setLED(0, 1, 0);
+      PT_WAIT_UNTIL(pt, millis() - timestamp > 1000 || interruptFlag);
+      timestamp = millis();
+      setLED(0, 0, 0);
+      PT_WAIT_UNTIL(pt, millis() - timestamp > 1000 || interruptFlag);
+      timestamp = millis();
+      break;
+    // BLINK RED
+    case 54:
+      setLED(0, 0, 1);
+      PT_WAIT_UNTIL(pt, millis() - timestamp > 1000 || interruptFlag);
+      timestamp = millis();
+      setLED(0, 0, 0);
+      PT_WAIT_UNTIL(pt, millis() - timestamp > 1000 || interruptFlag);
+      timestamp = millis();
+      break;
+    // BLINK GREEN
+    case 55:
+      setLED(1, 0, 0);
+      PT_WAIT_UNTIL(pt, millis() - timestamp > 1000 || interruptFlag);
+      timestamp = millis();
+      setLED(0, 0, 0);
+      PT_WAIT_UNTIL(pt, millis() - timestamp > 1000 || interruptFlag);
+      timestamp = millis();
+      break;
     // RED ONLY
     case 58:
       setLED(0, 0, 1);
@@ -72,12 +99,6 @@ static int ledController(struct pt *pt) {
     // GREEN ONLY
     case 59:
       setLED(1, 0, 0);
-      break;
-    case 168:
-      break;
-    case 144:
-      break;
-    case 104:
       break;
     case 152:
       break;
@@ -116,48 +137,3 @@ static int remoteController(struct pt *pt) {
   }
   PT_END(pt);
 }
-
-
-// sets initial value for pins so that lights start as "off" 
-// digitalWrite(green, LOW);
-// digitalWrite(yellow, LOW);
-// digitalWrite(red, LOW);
-// if(receiver.decode(&results)){}
-// if(results.value == 0xFFA25D){
-//   // repeats normal cycle 25 times
-//   digitalWrite(green, HIGH);   // turns the green light on
-//   delay(20000);               // holds the green light on for 20 seconds
-//   digitalWrite(green, LOW);    // turns the green light off
-//   delay(600);               // slight pause between lights
-//   digitalWrite(yellow, HIGH);  //turns the yellow light on
-//   delay(4000); //holds the yellow light for 4 seconds (watch out for that red-light camera!)
-//   digitalWrite(yellow, LOW); //turns the yellow light off
-//   delay(600);  //slight pause between lights
-//   digitalWrite(red, HIGH);  //turns the red light on
-//   delay(20000);  //holds the red light on for 20 seconds
-//   digitalWrite(red, LOW);  //turns the red light off
-//   delay(600);  //slight pause between lights
-//   var++;}  //adds 1 to variable "var" for repeat count
- 
-//   // after 25 cycles above, the light switches to "power outage mode", flashing red
-//   delay(600); //slight delay
-//   var1=0; //resets variable "var1" to 0
-//   while(var1 < 120) {
-//     // repeats power outage cycle 120 times - 2 minutes
-//    digitalWrite(red, HIGH);
-//    delay(600);
-//    digitalWrite(red, LOW);
-//    delay(400);
-//    var1++;}
-// var = 0
-
-//  //switches to "late night cycle" flashing yellow for 2 minutes, similar to flashing red above
-//  var1=0;
-//   while(var1 < 120) {
-//    digitalWrite(yellow, HIGH);
-//    delay(600);
-//    digitalWrite(yellow, LOW);
-//    delay(400);
-//    var1++;}
-//    var = 0;
- //goes back to normal cycle at top and repeats forever 
